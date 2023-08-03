79110e4abd362836b3c80342420bac2df23b1e04
Genesis Longart <102156923+GenyLongart@users.noreply.github.com>
2023-08-03 21:17:39 +0000

    Agregando resplandor con useState

--- src/js/component/Semaforo.jsx
+++ src/js/component/Semaforo.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Semaforo = () => {
+  const [encendida,setEncendida] = useState('')
   return (
     <div className='contenedor'>
         <div className="barra"></div>
         <div className="marco">
-            <div className="rojo resplandor"></div>
-            <div className="amarillo"></div>
-            <div className="verde"></div>
+            <div className={"rojo " + (encendida === 'brillaR' ? 'resplandor' : '')} onClick={() => setEncendida('brillaR')}></div>
+            <div className={"amarillo " + (encendida === 'brillaA' ? 'resplandor' : '')} onClick={() => setEncendida('brillaA')}></div>
+            <div className={"verde " + (encendida === 'brillaV' ? 'resplandor' : '')} onClick={() => setEncendida('brillaV')}></div>
         </div>
     </div>
   )
